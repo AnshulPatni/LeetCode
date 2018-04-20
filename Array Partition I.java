c5b94cde52d85ff0dfc9df1b1815fa09ecfbc83e
AnshulPatni <anshulpatni19@gmail.com>
2018-04-20 13:58:49 -0700

    Array Partition I

---
+++ Array Partition I.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        if(nums.length == 0)
+            return 0;
+        if(nums.length == 1)    {
+            return nums[0];
+        }
+        int sum = 0;
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i+=2)   {
+            sum = sum + nums[i];
+        }
+        return sum;
+    }
+}
