fd243e91195e885bac446e8ddfbe3be4f670ba53
AnshulPatni <anshulpatni19@gmail.com>
2018-09-13 17:20:59 -0700

    Flipping An Image

---
+++ FlippingAnImage.java
@@ -0,0 +1,21 @@
+public class FlippingAnImage {
+
+    public static void main(String[] args) {
+        int arr1[][] = new int[][]{{1,1,0,0},{1,0,0,1},{0,1,1,1},{1,0,1,0}};
+        int arr2[][] = new int[][]{{1,1,0},{1,0,1},{0,0,0}};
+
+        int res1[][] = flipAndInvertImage(arr1);
+        int res2[][] = flipAndInvertImage(arr2);
+
+        System.out.println(res1);
+        System.out.println(res2);
+    }
+
+    public static int[][] flipAndInvertImage(int[][] A) {
+        for(int[] eacharr : A) {
+            for() {
+
+            }
+        }
+    }
+}
